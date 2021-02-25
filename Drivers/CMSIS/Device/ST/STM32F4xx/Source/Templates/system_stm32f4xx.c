7ac93e4e8c810a7b3210cfea745c4e1647c588cb
Denis Shelema <77775954+DenDeg@users.noreply.github.com>
2021-02-25 14:17:30 +0300

    Update system_stm32f4xx.c

--- Drivers/CMSIS/Device/ST/STM32F4xx/Source/Templates/system_stm32f4xx.c
+++ Drivers/CMSIS/Device/ST/STM32F4xx/Source/Templates/system_stm32f4xx.c
@@ -90,8 +90,9 @@
 /*!< Uncomment the following line if you need to relocate your vector Table in
      Internal SRAM. */
 /* #define VECT_TAB_SRAM */
-#define VECT_TAB_OFFSET  0x00 /*!< Vector Table base offset field. 
-                                   This value must be a multiple of 0x200. */
+#if !defined  (VECT_TAB_OFFSET)
+  #define VECT_TAB_OFFSET  0x00 /*!< Vector Table base offset field. This value must be a multiple of 0x200. */
+#endif /* VECT_TAB_OFFSET */
 /******************************************************************************/
 
 /**
